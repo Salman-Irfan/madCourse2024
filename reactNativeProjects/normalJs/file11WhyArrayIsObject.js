520435aa7c2aae971e8d73e65723988248b13db3
Salman Irfan <107026575+Salman-Irfan@users.noreply.github.com>
2024-07-23 15:38:14 +0500

    array is a special kind of object

---
+++ reactNativeProjects/normalJs/file11WhyArrayIsObject.js
@@ -0,0 +1,13 @@
+const labRoom = {
+    num: `cs-305`,
+    systems: 50,
+    isProjector: true,
+    0: `number a s a key`,
+}
+
+console.log(labRoom)
+
+for (let i in labRoom){
+    console.log(i)
+    console.log(labRoom[i])
+}
