b41a99570a1c932c718bd29584b6f95b60e69b2e
Salman Irfan <107026575+Salman-Irfan@users.noreply.github.com>
2024-07-23 15:23:25 +0500

    clone array and objects with 3 dots spread operator

---
+++ reactNativeProjects/normalJs/file10cloneArrayObject.js
@@ -0,0 +1,13 @@
+const uolDepartemnts = [`cs`, `se`, `ai`, `soca`, `mbbs`]
+
+const otherUni = [`mass comm`, `physics` ,...uolDepartemnts, `arts`, `english`]
+console.log(otherUni)
+
+
+const sohaib = {
+    name: `sohaib`,
+    age: 21,
+}
+
+const hamza = {...sohaib, isGlasses: false}
+console.log(hamza)
