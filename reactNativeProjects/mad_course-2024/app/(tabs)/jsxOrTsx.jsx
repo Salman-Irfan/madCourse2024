4a8255b4abeb6ad4ce08fe03cff2911a70602cc4
Salman Irfan <107026575+Salman-Irfan@users.noreply.github.com>
2024-08-19 10:13:28 +0500

    tsx vs jsx

---
+++ reactNativeProjects/mad_course-2024/app/(tabs)/jsxOrTsx.jsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const JsxOrTsx = () => {
+    let str = `abc`
+    console.log(str)
+    return (
+        <View>
+            <Text>JsxOrTsx</Text>
+        </View>
+    )
+}
+
+export default JsxOrTsx
+
+const styles = StyleSheet.create({})
---
+++ reactNativeProjects/mad_course-2024/app/(tabs)/tsxOrJsx.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const TsxOrJsx = () => {
+    let confusingStr:number = `12345`
+    console.log(confusingStr)
+    return (
+        <View>
+            <Text>TsxOrJsx</Text>
+        </View>
+    )
+}
+
+export default TsxOrJsx
+
+const styles = StyleSheet.create({})
